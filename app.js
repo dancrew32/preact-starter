a74333b4fe4f91ff964ca642813d2bd3ac0a750d
Dan Masquelier <dancrew32@gmail.com>
2017-09-12 21:39:21 -0700

    separate app

---
+++ app.js
@@ -0,0 +1,40 @@
+const {h, render, Component} = window.preact;
+
+const Button = (props) => (
+	h('button', {onClick: props.onClick}, props.text)
+);
+
+const Hello = (props) => (
+	h('div', null, [ 
+		Button(props),
+		`hello ${props.ip}`
+	])
+);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ip: '',
+			count: 0
+		};
+		this.increment = this.increment.bind(this);
+	}
+	async componentDidMount() {
+		const res = await fetch('https://httpbin.org/get');
+		const json = await res.json();
+	  this.setState({ip: json.origin});
+	}
+	increment() {
+		this.setState({count: this.state.count + 1});
+	}
+	render() {
+		return Hello({
+			text: this.state.count,
+			onClick: this.increment,
+			ip: this.state.ip
+		});
+	}
+};
+
+render(h(App), document.getElementById('app'));
